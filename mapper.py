efd0e3473db9bad1b6ff90159a4082ed4373dd65
Mark Lowerison <mark@lowerison.com>
2014-02-19 08:47:58 -0700

    Key errors

--- mapper.py
+++ mapper.py
@@ -4,27 +4,28 @@ import json
 import sys
 
 dataFile = open('data/wide_data.json')
-rc = 2
 
+lc = 0
 for line in dataFile:
+    lc += 1
+print lc
+
 #for line in sys.stdin:
-    try:
+for line in dataFile:
+    record = json.loads(line)
+    data = record[1]
+    mc = len(data+1)
+    print mc
+    totalExpression = 0
+    for j in data:
+        totalExpression += int(j)
+    mean = float(totalExpression)/len(data)
+    
+    for i in range(1,lc):
+        print i
+        for j in range(1,mc):
+            print j
+            # print "(%s,%s)\t(%s,%s,%s,%s)" % (record[0], i, "'a'", record[0], j+1, int(record[1][j]) )
+            # print "(%s,%s)\t(%s,%s,%s,%s)" % (i, record[0], "'b'", j+1, record[0], int(record[1][j]) )
 
-        record = json.loads(line)
-        data = record[1]
-        mc = len(data)
-        # key: position in nxn matrix
-        # value: row
-        totalExpression = 0
-        for j in data:
-            totalExpression += int(j)
-        mean = float(totalExpression)/len(data)
-        
-        for i in range(rc+1):
-            for j in range(mc+1):
-                print "(%s,%s)\t(%s,%s,%s,%s)" % (record[0], j+1, "'a'", record[0], j+1, int(record[1][j]) )
-                print "(%s,%s)\t(%s,%s,%s,%s)" % (j+1, record[0], "'b'", j+1, record[0], int(record[1][j]) )
-
-    except:
-        pass
         
