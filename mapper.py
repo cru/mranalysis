e81781d59c78ddfbfc4181636df2d6babe3da4eb
Jordan Engbers <jdtengbe@ucalgary.ca>
2014-02-19 08:53:11 -0700

    Fixed mapper and reducer

--- mapper.py
+++ mapper.py
@@ -8,23 +8,19 @@ rc = 2
 
 for line in dataFile:
 #for line in sys.stdin:
-    try:
 
-        record = json.loads(line)
-        data = record[1]
-        mc = len(data)
-        # key: position in nxn matrix
-        # value: row
-        totalExpression = 0
-        for j in data:
-            totalExpression += int(j)
-        mean = float(totalExpression)/len(data)
-        
-        for i in range(rc+1):
-            for j in range(mc+1):
-                print "(%s,%s)\t(%s,%s,%s,%s)" % (record[0], j+1, "'a'", record[0], j+1, int(record[1][j]) )
-                print "(%s,%s)\t(%s,%s,%s,%s)" % (j+1, record[0], "'b'", j+1, record[0], int(record[1][j]) )
+    record = json.loads(line)
+    data = record[1]
+    mc = len(data)
+    # key: position in nxn matrix
+    # value: row
+    totalExpression = 0
+    for j in data:
+        totalExpression += int(j)
+    mean = float(totalExpression)/len(data)
+    for i in range(1, rc+1):
+        for j in range(0, mc):
+            print "(%s,%s)\t(%s,%s,%s,%s)" % (record[0], i, "'a'", record[0], j+1, int(record[1][j]) )
+            print "(%s,%s)\t(%s,%s,%s,%s)" % (i, record[0], "'b'", j+1, record[0], int(record[1][j]) )
 
-    except:
-        pass
         
--- reducer.py
+++ reducer.py
@@ -7,20 +7,34 @@ import sys
 a = []
 b = []
 cellSum = 0
+last_rec = None
 for line in sys.stdin:
     record = line.strip().split("\t")
     key = eval(record[0])
+    if not last_rec:
+        last_rec = key
     rec = eval(record[1])
-    a.append(rec) if rec[0] == 'a' else b.append(rec)
+    if key == last_rec:
+        a.append(rec) if rec[0] == 'a' else b.append(rec)
+    else:
+        for row in a:
+            for col in b:
+                if row[2] == col[1]:
+                    cellSum += row[3]*col[3]
+        print cellSum
+        a = []
+        b = []
+        cellSum = 0
+        last_rec = key
+        a.append(rec) if rec[0] == 'a' else b.append(rec)
+for row in a:
+        for col in b:
+            if row[2] == col[1]:
+                cellSum += row[3]*col[3]
+print cellSum
 
     
-for row in a:
-    for col in b:
-    
-        if row[1] == col[2]:
-            cellSum += row[3]*col[3]
-    print cellSum
-    #         cellSum += row[3] * col[3]; 
+   #         cellSum += row[3] * col[3]; 
     # print '%s\t%s\t%s' % (str(key[0]), str(key[1]), str(cellSum)) 
 
     
